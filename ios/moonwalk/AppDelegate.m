197d2e45dae5af269d0904fdd136fe89bf94046e
huyhomie66 <huyhomie66@gmail.com>
2020-11-13 03:38:18 +0700

    init

--- ios/moonwalk/AppDelegate.m
+++ ios/moonwalk/AppDelegate.m
@@ -12,14 +12,11 @@
 #import <React/RCTRootView.h>
 
 @import UIKit;
-@import Firebase;
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [FIRApp configure];
-  
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"moonwalk"
